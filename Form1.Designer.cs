201bce8c06533a7d4b3b935233dbf7f7fe25666c
fil16061979 <m_f_n@ukr.net>
2021-02-21 14:22:33 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace MyBrouzer1
+{
+    partial class addrBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addrBox));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.prevPage = new System.Windows.Forms.Button();
+            this.nextPage = new System.Windows.Forms.Button();
+            this.homePage = new System.Windows.Forms.Button();
+            this.goPage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 450);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление каталогом";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.goPage);
+            this.groupBox2.Controls.Add(this.homePage);
+            this.groupBox2.Controls.Add(this.nextPage);
+            this.groupBox2.Controls.Add(this.prevPage);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(294, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(632, 62);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Адресная строка";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.webBrowser1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(294, 62);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(632, 388);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Панель просмотра";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(288, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(3, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 405);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // prevPage
+            // 
+            this.prevPage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.prevPage.Location = new System.Drawing.Point(3, 18);
+            this.prevPage.Name = "prevPage";
+            this.prevPage.Size = new System.Drawing.Size(75, 41);
+            this.prevPage.TabIndex = 0;
+            this.prevPage.Text = "<<<";
+            this.prevPage.UseVisualStyleBackColor = true;
+            this.prevPage.Click += new System.EventHandler(this.prevPage_Click);
+            // 
+            // nextPage
+            // 
+            this.nextPage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.nextPage.Location = new System.Drawing.Point(78, 18);
+            this.nextPage.Name = "nextPage";
+            this.nextPage.Size = new System.Drawing.Size(75, 41);
+            this.nextPage.TabIndex = 1;
+            this.nextPage.Text = ">>>";
+            this.nextPage.UseVisualStyleBackColor = true;
+            this.nextPage.Click += new System.EventHandler(this.nextPage_Click);
+            // 
+            // homePage
+            // 
+            this.homePage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.homePage.Location = new System.Drawing.Point(153, 18);
+            this.homePage.Name = "homePage";
+            this.homePage.Size = new System.Drawing.Size(75, 41);
+            this.homePage.TabIndex = 2;
+            this.homePage.Text = "Home";
+            this.homePage.UseVisualStyleBackColor = true;
+            this.homePage.Click += new System.EventHandler(this.homePage_Click);
+            // 
+            // goPage
+            // 
+            this.goPage.Dock = System.Windows.Forms.DockStyle.Right;
+            this.goPage.Location = new System.Drawing.Point(554, 18);
+            this.goPage.Name = "goPage";
+            this.goPage.Size = new System.Drawing.Size(75, 41);
+            this.goPage.TabIndex = 3;
+            this.goPage.Text = "Go";
+            this.goPage.UseVisualStyleBackColor = true;
+            this.goPage.Click += new System.EventHandler(this.goPage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(228, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 18);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(626, 367);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("/www.bing.com", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // addrBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "addrBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Браузер с каталогом сайтов";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button goPage;
+        private System.Windows.Forms.Button homePage;
+        private System.Windows.Forms.Button nextPage;
+        private System.Windows.Forms.Button prevPage;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyBrouzer1
+{
+    public partial class addrBox : Form
+    {
+        Database1Entities db = new Database1Entities();
+
+        public addrBox()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadCategories()
+        {
+            var categoriesList = db.Categories.ToList();
+            comboBox1.Items.Clear();
+            comboBox1.DataSource = categoriesList;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.bing.com");
+            LoadCategories();
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            addrBox.Text = webBrowser1.Url.ToString();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            addrBox.SelectAll();
+        }
+
+        private void GoToSite()
+        {
+            string address = addrBox.Text;
+            if(address=="")
+            {
+                MessageBox.Show("Не указан адрес страницы", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (address.IndexOf(".")==-1)
+                {
+                    address = $"https://www.google.com/search?q={address}";
+                }
+                else if(address.IndexOf("http")==-1)
+                {
+                    address = $"https://{address}";
+                }
+                webBrowser1.Navigate(address);
+            }
+        }
+        
+
+        private void goPage_Click(object sender, EventArgs e)
+        {
+            GoToSite();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                GoToSite();
+        }
+
+        private void prevPage_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void nextPage_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        }
+
+        private void homePage_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.GoHome();
+            webBrowser1.Navigate("https://www.bing.com");
+        }
+
+        private void comboBox1_SelectedIndexChange(object sender, EventsArgs e)
+        {
+            int K = comboBox1.SelectedIndex;
+            if (K !=-1)
+            {
+                var sitesList = (comboBox1.SelectedItem as Categories).Sites.ToList();
+                listBox1.Items.Clear();
+                listBox1.DataSource = sitesList;
+                listBox1.DisplayMember = "Name";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int k = listBox1.SelectedIndex;
+            if (k !=-1)
+            {
+                string url = (listBox1.SelectedItem as Sites).Address;
+                webBrowser1.Navigate(url);
+            }
+        }
+    }
+}
